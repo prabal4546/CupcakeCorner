dd5c57ae9f2cb7675d34104df67732764a3d76bb
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-10-28 22:46:23 +0530

    checkout ui complete

--- QuazyCupcake/View/CheckoutView.swift
+++ QuazyCupcake/View/CheckoutView.swift
@@ -10,9 +10,27 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order:Order
-
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{geo in
+            ScrollView{
+                VStack{
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:geo.size.width)
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+                    
+                    Button("Place Order") {
+                        // place the order
+                    }
+                    .padding()
+                }
+                
+            }
+        }
+        .navigationBarTitle("Check Out", displayMode: .inline)
     }
 }
 
